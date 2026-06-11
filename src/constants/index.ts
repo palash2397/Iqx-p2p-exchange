66c008293db0ef902d0d72b57b6016aa0c0b98ca
palash2397 <palashpatil55@gmail.com>
2026-06-11 15:20:37 +0530

    feat: configure swagger docs, cors, morgan logging, and validation pipes

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export default {
+  SWAGGER: {
+    TITLE: 'IQX P2P Exchange API',
+    DESCRIPTION: 'API documentation for IQX P2P Exchange System',
+    VERSION: '1.0',
+    SERVER_URL: '',
+  },
+
+  Global: {
+    PREFIX: '/api/v1',
+  },
+};
